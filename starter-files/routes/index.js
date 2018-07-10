5b51d8ad8a930c22aa911e959afe5f672709d46e
John <entity.john@gmail.com>
2018-07-10 12:01:37 -0500

    getting started, add env variables

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,14 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+	res.render('hello', {
+		name: 'John',
+		cat: req.query.cat,
+	});
+});
+
+router.get('/reverse/:name', (req, res) => {
+	res.send([...req.params.name].reverse().join(''));
 });
 
 module.exports = router;
