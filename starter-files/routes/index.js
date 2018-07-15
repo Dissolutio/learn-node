b9861816f8ff13d3a657160ba518198d0ce13b73
John <entity.john@gmail.com>
2018-07-15 00:14:06 -0500

    add store model

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/', (req, res) => {
-	res.render('hello', {
-		name: 'John',
-		cat: req.query.cat,
-	});
-});
-
-router.get('/reverse/:name', (req, res) => {
-	res.send([...req.params.name].reverse().join(''));
-});
-
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
 module.exports = router;
